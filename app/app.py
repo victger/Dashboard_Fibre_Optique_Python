2e5c1528d8582fcb355201f90285f3085caa2022
vgvictorgerard@gmail.com <vgvictorgerard@gmail.com>
2024-12-03 13:44:48 +0100

    Remove useless code

--- app/app.py
+++ app/app.py
@@ -5,15 +5,12 @@ from app.callbacks import register_callbacks
 
 def launch_app(df_region, df_departement, df_commune, d):
 
-    # Initialisation de l'application Dash
     app = Dash(__name__)
 
     create_map(df_commune)
 
-    # Définition de la mise en page en passant les DataFrames nécessaires
     app.layout = create_layout(df_region, d)
 
-    # Enregistrement des callbacks en passant les DataFrames
     register_callbacks(app, df_departement, d)
     
     app.run_server(host="0.0.0.0", port=8050)
